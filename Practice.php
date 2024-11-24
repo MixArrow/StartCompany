36f563b9b2346fa44f27a58ef3758bdfdd1108ef
Arrow <149740138+MixArrow@users.noreply.github.com>
2024-11-24 02:04:28 -0500

    Update Practice.php

--- Practice.php
+++ Practice.php
@@ -1,9 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>¡Hola, Mundo desde PHP!</title>
+    <title>Calculadora</title>
 </head>
 <body>
+    <h2>Calculadora</h2>
+    <form method= "post" action="">
+        <input type="number" name="num1" placeholder="Number 1">
+        <input type="number" name="num2" placeholder="Number 2">
+        <select name="Math Operation">
+
+            <option value="Addition">Addition</option>
+            <option value="Subtraction">Subtraction</option>
+            <option value="Division">Division</option>
+            <option value="Multiplication">Multiplication</option>
+        </select>
+
+        <button type="Submit">Calculando</button>
+        
+    </form>
+    
     <?php
     echo "<h1>¡Bienvenido a tu primer programa en PHP!</h1>";
     echo "<p>Este es un simple ejemplo que muestra un mensaje en la pantalla.</p>";
