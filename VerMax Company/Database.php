ede7271a230c7f866b6fb67edce8b9d3ac9fe803
Arrow <149740138+MixArrow@users.noreply.github.com>
2024-11-26 02:01:36 -0500

    Update Database.php

---
+++ VerMax Company/Database.php
@@ -0,0 +1,9 @@
+<?php
+// Variables para el ejemplo (puedes usar una base de datos más adelante)
+$usuario_correcto = "admin";
+$contrasena_correcta = "1234";
+
+// Obtener los datos del formulario
+$username = $_POST['username'] ?? '';
+$password = $_POST['password'] ?? '';
+?>
