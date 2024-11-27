7e5b008a85721bbcec4b789cc6f936779bbe8b0d
Arrow <149740138+MixArrow@users.noreply.github.com>
2024-11-27 00:13:16 -0500

    Update Frontend.php

--- VerMax Company/Frontend.php
+++ VerMax Company/Frontend.php
@@ -3,14 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>VerMax Company</title>
-    <link rel ="stylesheet" href ="styles.css">
+    <title>VerMax LOQ</title>
+    <link rel="stylesheet" href="Frontend.css">
 </head>
 <body>
     <!-- Header -->
     <header>
-        <div class="logo">VerMax Company</div>
+        <div class="logo">Vermax Company</div>
         <div class="login">
             <a href="#">Start Login</a>
         </div>
+        <div class="fondo-animado"></div>
     </header>
